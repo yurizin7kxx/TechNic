ddc6bdd520cd06e2b01e737a0ec685c9ab9d4e5d
yurizin7kxx <tercioyuri15@gmail.com>
2026-03-24 10:33:12 -0300

    Update: Adicionando .gitignore e limpando arquivos desnecessários

--- .next/dev/server/pages/_document.js
+++
@@ -1,5 +0,0 @@
-var R=require("../chunks/ssr/[turbopack]_runtime.js")("server/pages/_document.js")
-R.c("server/chunks/ssr/node_modules_186c80ea._.js")
-R.c("server/chunks/ssr/[root-of-the-server]__aa0b05e3._.js")
-R.m("[project]/src/pages/_document.js [ssr] (ecmascript)")
-module.exports=R.m("[project]/src/pages/_document.js [ssr] (ecmascript)").exports
--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"hAARgqy8hL/RlpabgDOzjdYKe8N7bJtlLOA9iJJmAE8=\"\n}"
