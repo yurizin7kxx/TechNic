38222a0679aab30d0f845bc05efc0d280f0363e6
yurizin7kxx <tercioyuri15@gmail.com>
2026-03-29 18:23:24 -0300

    removi a pages hello

--- src/pages/hello.js
+++
@@ -1,6 +0,0 @@
-export const dynamic = 'force-dynamic';
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-export default function handler(req, res) {
-  res.status(200).json({ name: "John Doe" });
-}
